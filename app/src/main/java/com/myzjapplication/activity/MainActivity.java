aee95f6d6ad21ffc067db7450d36c60d8fed991a
zhengdan <zhengdan_nacy@163.com>
2019-07-03 11:19:55 +0800

    查看手机多媒体数据库中的音频

--- app/src/main/java/com/myzjapplication/activity/MainActivity.java
+++ app/src/main/java/com/myzjapplication/activity/MainActivity.java
@@ -1,21 +1,40 @@
 package com.myzjapplication.activity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.myzjapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
+    public static String TAG ="audioInfo";
     private Button mBtn;
+    private Button mGetAudioBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mBtn = findViewById(R.id.goto_show);
+        mGetAudioBtn = findViewById(R.id.get_audio_info_btn);
         mBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,5 +42,116 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        mGetAudioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkPermissionStore(MainActivity.this)) {
+                    getExternalAudioInfo();
+                    geInternalAudioInfo();
+                }
+
+            }
+        });
+    }
+
+
+    public void getExternalAudioInfo() {
+        //存储在sd卡上的音频文件
+        Cursor cursor = this.getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                null, null, null, MediaStore.Audio.Media.DEFAULT_SORT_ORDER);
+        cursor.moveToFirst();
+        int counter = cursor.getCount();
+        String title1 = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE));
+
+        Log.w(TAG, "存储在sd卡上的音频文件------------before looping, title = " + title1);
+        for (int j = 0; j < counter; j++) {
+
+            String title =  cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
+            String displayName = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME));
+            String size =  cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.SIZE));
+            Log.w(TAG, "title     " + title +"displayName     " +displayName + "size     " +size);
+            cursor.moveToNext();
+        }
+        cursor.close();
+    }
+
+    public void geInternalAudioInfo() {
+        //存储在手机内部存储器上
+        Cursor cursor = this.getContentResolver().query(MediaStore.Audio.Media.INTERNAL_CONTENT_URI ,
+                null, null, null, MediaStore.Audio.Media.DEFAULT_SORT_ORDER);
+        cursor.moveToFirst();
+        int counter = cursor.getCount();
+        String title1 = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE));
+
+        Log.w(TAG, "存储在手机内部存储器上------------before looping, title = " + title1);
+        for (int j = 0; j < counter; j++) {
+
+            String title =  cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
+            String displayName = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME));
+            String size =  cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.SIZE));
+            Log.w(TAG, "title     " + title +"displayName     " +displayName + "size     " +size);
+            cursor.moveToNext();
+        }
+        cursor.close();
+    }
+
+
+    public boolean checkPermissionStore(Context context){
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    showDialog("External storage", context, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+                } else {
+                    ActivityCompat.requestPermissions(
+                                    (Activity) context,
+                                    new String[] { Manifest.permission.READ_EXTERNAL_STORAGE },
+                                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+
+        } else {
+            return true;
+        }
+    }
+
+    public void showDialog(final String msg, final Context context,
+                           final String permission) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle("Permission necessary");
+        alertBuilder.setMessage(msg + " permission is necessary");
+        alertBuilder.setPositiveButton(android.R.string.yes,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions((Activity) context,
+                                new String[] { permission },
+                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                    }
+                });
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getExternalAudioInfo();
+                    geInternalAudioInfo();
+                } else {
+                    Toast.makeText(MainActivity.this, "GET_ACCOUNTS Denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions,
+                        grantResults);
+        }
     }
 }
