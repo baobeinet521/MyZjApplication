e48ca44898d46b124954f31ab6d4a34798834597
zhengdan <zhengdan_nacy@163.com>
2019-07-04 17:35:25 +0800

    调用系统的方法打开文件管理器和相关音频软件

---
+++ app/src/main/java/com/myzjapplication/FileDataBean.java
@@ -0,0 +1,49 @@
+package com.myzjapplication;
+
+public class FileDataBean {
+    private String fileName;
+    private String filePath;
+    private String fileSizeHasCompany;
+    private String fileSizeNoCompany;
+    private String fileTime;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFileSizeHasCompany() {
+        return fileSizeHasCompany;
+    }
+
+    public void setFileSizeHasCompany(String fileSizeHasCompany) {
+        this.fileSizeHasCompany = fileSizeHasCompany;
+    }
+
+    public String getFileSizeNoCompany() {
+        return fileSizeNoCompany;
+    }
+
+    public void setFileSizeNoCompany(String fileSizeNoCompany) {
+        this.fileSizeNoCompany = fileSizeNoCompany;
+    }
+
+    public String getFileTime() {
+        return fileTime;
+    }
+
+    public void setFileTime(String fileTime) {
+        this.fileTime = fileTime;
+    }
+}
